bac09de6d0bf70ffbe00c8c5a4675f66ae6d5d7d
Sachin235446 <sachinkumar.112538@gmail.com>
2026-09-02 20:13:10 +0530

    no error

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+
 import 'screens/home_page.dart';
 
 void main() {
@@ -22,7 +23,7 @@ class MausamApp extends StatelessWidget {
           brightness: Brightness.dark,
         ),
       ),
-      home: const HomePage(),
+      home: HomePage(),
     );
   }
 }
